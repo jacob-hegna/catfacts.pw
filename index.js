d8cc47c5bf890300256e21cd76953c2bb0105fe9
Jacob Hegna <jacobhegna@gmail.com>
2014-12-01 17:19:36 -0600

    properly register phone numbers over POST

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser'),
+    phone = require('phone');
 
 app.set('views', 'views')
 app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.render('index', {});
 });
 
+app.get('/success', function(req, res) {
+    res.render('index');
+});
+
 app.post('/register', function(req, res) {
-    console.log(req);
+    var number = phone(req.body.number, 'USA')[0];
+    number = number.slice(2, number.length);
+    res.redirect('/success');
 });
 
 app.get('*', function(req, res) {
     res.render('error', {code: 404});
 });
 
-app.listen(8080);
+app.listen(80);
