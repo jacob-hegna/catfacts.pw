337dea4abbfbdaa8e067270cfe76e80b93be6f33
Jacob Hegna <jacobhegna@gmail.com>
2014-12-01 21:59:02 -0600

    mongodb added

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    phone = require('phone');
+    phone = require('phone'),
+    nodemailer = require('nodemailer'),
+    config = require('config'),
+    mongo = require('mongojs'),
+    db = mongo('catfacts', ['users']);
 
 app.set('views', 'views')
 app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var extensions = {
+    verizon: '@vtext.com',
+    sprint: '@pm.sprint.com',
+    att: '@txt.att.net',
+    tmobile: '@tmomail.net'
+};
+
+var transporter = nodemailer.createTransport({
+    service: config.get('service'),
+    auth: {
+        user: config.get('email'),
+        pass: config.get('password')
+    }
+});
 
 app.get('(/|/home/?)', function(req, res) {
     res.render('index', {nav: 0});
@@ -22,13 +41,21 @@ app.get('/login/?', function(req, res) {
 });
 
 app.get('/success/?', function(req, res) {
-    res.render('index', {nav: -1});
+    res.render('success', {nav: -1});
 });
 
 app.post('/register/?', function(req, res) {
     var number = phone(req.body.number, 'USA')[0];
+    var carrier = req.body.carrier;
     number = number.slice(2, number.length);
-    res.redirect('/success');
+    transporter.sendMail({
+        from: config.get('email'),
+        to: (number + extensions[carrier]),
+        subject: '',
+        text: 'Welcome to catfacts! Have fun learning :)'
+    });
+    db.users.save({number: number, carrier: carrier});
+    res.redirect('/success/');
 });
 
 app.get('*', function(req, res) {
