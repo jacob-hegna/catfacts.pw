fa3a584ae4c8eb405acf02436a832de9d5a54700
Jacob Hegna <jacobhegna@gmail.com>
2014-12-01 20:30:46 -0600

    added a navbar

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.render('index', {});
+app.get('(/|/home/?)', function(req, res) {
+    res.render('index', {nav: 0});
 });
 
-app.get('/success', function(req, res) {
-    res.render('index');
+app.get('/about/?', function(req, res) {
+    res.render('about', {nav: 1});
 });
 
-app.post('/register', function(req, res) {
+app.get('/login/?', function(req, res) {
+    res.render('login', {nav: 2});
+});
+
+app.get('/success/?', function(req, res) {
+    res.render('index', {nav: -1});
+});
+
+app.post('/register/?', function(req, res) {
     var number = phone(req.body.number, 'USA')[0];
     number = number.slice(2, number.length);
     res.redirect('/success');
